01c5453e93633f4f34187bceda2a9461e51a6863
David Rawson <dm.rawson@gmail.com>
2018-04-09 19:05:08 +0100

    Complete.

--- raindrops.rb
+++ raindrops.rb
@@ -1,7 +1,25 @@
-class Rainrops
+class Raindrops
 
   def self.convert(number)
+    raindrop_sound = ""
 
+    if number % 3 == 0
+      raindrop_sound.concat("Pling")
+    end
+
+    if number % 5 == 0
+      raindrop_sound.concat("Plang")
+    end
+
+    if number % 7 == 0
+      raindrop_sound.concat("Plong")
+    end
+    
+    if raindrop_sound == ""
+      raindrop_sound = number.to_s
+    end
+
+    return raindrop_sound
   end
 
 end
